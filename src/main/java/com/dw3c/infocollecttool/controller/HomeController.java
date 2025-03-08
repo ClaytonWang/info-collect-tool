19e538df21ac46d3c2f6ffda7a5785419acf8203
Clayton Wang <lovejunlic@qq.com>
2025-03-08 21:15:22 +0800

    feat(summary): 添加信息汇总功能并修改表格样式

--- src/main/java/com/dw3c/infocollecttool/controller/HomeController.java
+++ src/main/java/com/dw3c/infocollecttool/controller/HomeController.java
@@ -1,11 +1,19 @@
 package com.dw3c.infocollecttool.controller;
 
+import com.dw3c.infocollecttool.entity.InfoCollection;
+import com.dw3c.infocollecttool.service.IInfoCollectionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    private IInfoCollectionService infoCollectionService;
 
     @GetMapping("/")
     public String index() {
@@ -14,6 +22,8 @@ public class HomeController {
 
     @GetMapping("/summary")
     public String info(Model model) {
+        List<InfoCollection> list = infoCollectionService.getAll();
+        model.addAttribute("list", list);
         return "summary";
     }
 
